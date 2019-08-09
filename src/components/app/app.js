f821cc4f29fb789a8c900939685f5817b0261c22
Sirji <postoldster@gmail.com>
2019-08-09 17:33:53 +0300

    First commit at Todo_App

---
+++ src/components/app/app.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+
+import TodoList from "../todo-list";
+import AppHeader from "../app-header";
+import SearchPanel from "../search-panel";
+import ItemStatusFilter from "../item-status-filter";
+import ItemAddForm from '../item-add-form';
+
+import "./app.css";
+
+export default class App extends Component {
+
+  maxId = 100;
+
+  state = {
+    todoData: [
+      this.createTodoItem("Drink Coffee"),
+      this.createTodoItem("Make Awesome App"),
+      this.createTodoItem("Have a lunch"),
+    ]
+  };
+
+  createTodoItem (label) {
+    return {
+      label,
+      important: false,
+      done: false,
+      id: this.maxId++
+    }
+  };
+
+  addItem = (text) => {
+    //generate uniq id
+    const newItem = this.createTodoItem(text);
+
+    //add new element to array
+    this.setState(({todoData}) => {
+      const newArr = [...todoData, newItem];
+
+      return {
+        todoData: newArr
+      };
+    });
+  };
+
+  toggleProperty(arr, id, propName) {
+    const idx = arr.findIndex((el) => el.id === id);
+      //update object
+      const oldItem = arr[idx];
+      const newItem = {...oldItem,
+          [propName]: !oldItem[propName]};
+      
+      return [
+        ...arr.slice(0, idx),
+        newItem,
+        ...arr.slice(idx + 1)
+      ];
+  };
+
+  onToggleImportant = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, 'important')
+      };
+    });
+  };
+
+  onToggleDone = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, 'done')
+      };
+    });
+  };
+
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex(el => el.id === id);
+
+      const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+
+      return {
+        todoData: newArray
+      };
+    });
+  };
+
+
+  render() {
+
+    const doneCount = this.state.todoData
+                      .filter((el) => el.done).length;
+    const todoCount = this.state.todoData.length - doneCount;
+
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={todoCount} done={doneCount} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <TodoList 
+          todos={this.state.todoData} 
+          onDeleted={this.deleteItem}
+          onToggleImportant={this.onToggleImportant}
+          onToggleDone={this.onToggleDone} />
+        <ItemAddForm onItemAdded = { this.addItem }/>
+      </div>
+    );
+  }
+}
---
+++ src/components/item-add-form/item-add-form.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import './item-add-form.css'; 
+
+export default class ItemAddForm extends Component {
+
+  state = {
+    label: ''
+  };
+
+  //e => event
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+    this.setState({
+      label: ''
+    });
+  };
+
+  render() {
+    return (
+    <form className="item-add-form d-flex"
+          onSubmit={this.onSubmit} >
+      <input type='text'
+              className='form-controls'
+              onChange={this.onLabelChange}
+              placeholder='What need to be done'
+              value={this.state.value} />
+      <button 
+        className="btn btn-outline-secondary" >
+        Add Item
+      </button>
+    </form>
+    );
+  };
+};
+
+
